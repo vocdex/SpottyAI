e10aa52553545368408bd9cdbaff4b638f3982e8
Shukrullo Nazirjonov <nazirjonovsh2000@gmail.com>
2024-12-29 12:16:52 +0500

    refactor, and add initial code for action-enabled robot assistant

--- spotty/spotty/audio/__init__.py
+++ spotty/spotty/audio/__init__.py
@@ -1,2 +1,2 @@
-from .spot_assistant import WakeWordConversationAgent
+from .spot_assistant import WakeWordConversationAgent, initialize_pygame_mixer, get_abs_path
 from .system_prompts import *
--- spotty/spotty/audio/spot_assistant.py
+++ spotty/spotty/audio/spot_assistant.py
@@ -6,7 +6,6 @@ import wave
 import requests
 import threading
 import queue
-import argparse
 import subprocess
 import platform
 import time
@@ -15,8 +14,23 @@ import numpy as np
 import pygame
 from pvrecorder import PvRecorder
 from dotenv import load_dotenv
-
 load_dotenv()  
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+TEMP_FILE = "output.wav"
+
+# Initialize PyAudio
+audio = pyaudio.PyAudio()
+
+# Audio settings
+RATE = 16000
+CHUNK = 1024
+
+# Configuration and API settings
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+PICOVOICE_ACCESS_KEY = os.getenv("PICOVOICE_ACCESS_KEY")
+
 
 try:
     from whispercpp import Whisper
@@ -27,19 +41,6 @@ except ImportError:
     WHISPER_LOCAL_AVAILABLE = False
     LLAMA_LOCAL_AVAILABLE = False
 
-# Configuration and API settings
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-PICOVOICE_ACCESS_KEY = os.getenv("PICOVOICE_ACCESS_KEY")
-
-# Audio settings
-RATE = 16000
-CHUNK = 1024
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-TEMP_FILE = "output.wav"
-
-# Initialize PyAudio
-audio = pyaudio.PyAudio()
 
 # Chat history
 chat_history = []
@@ -366,7 +367,7 @@ class WakeWordConversationAgent:
         )
         self.audio_device_index = audio_device_index
         self.system_prompt = system_prompt
-        
+
         # Configuration for transcription and chat
         self.transcription_method = transcription_method
         self.inference_method = inference_method
@@ -545,43 +546,3 @@ def get_abs_path(relative_path:str):
     if not os.path.isabs(relative_path):
         return os.path.abspath(relative_path)
     return relative_path
-
-def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser(description="Wake Word Conversation Agent")
-    parser.add_argument('--keyword-model', required=False, default="./hey_spot_version_02/Hey-Spot_en_mac_v3_0_0.ppn", help='Path to wake word model')
-    parser.add_argument('--transcribe', choices=['openai', 'local'], default='openai')
-    parser.add_argument('--whisper-model', choices=['tiny', 'base', 'small', 'medium', 'large'], default='tiny')
-    parser.add_argument('--chat', choices=['openai', 'local'], default='openai')
-    parser.add_argument('--llama-model', type=str, default="./models/mistral-7b-instruct-v0.1.Q4_K_S.gguf")
-    parser.add_argument('--tts', type=str, choices=['openai', 'local'], default='openai')
-    parser.add_argument('--audio-device-index', type=int, default=-1)
-    
-    args = parser.parse_args()
-    initialize_pygame_mixer()
-
-    from spotty.audio import system_prompt_assistant
-    # Initialize and start the conversation agent
-    agent = WakeWordConversationAgent(
-        access_key=PICOVOICE_ACCESS_KEY,
-        system_prompt=system_prompt_assistant,
-        keyword_path=get_abs_path(args.keyword_model),
-        transcription_method=args.transcribe,
-        inference_method=args.chat,
-        local_whisper_model=args.whisper_model,
-        local_llama_model=get_abs_path(args.llama_model),
-        tts = args.tts,
-        audio_device_index=args.audio_device_index
-    )
-    
-    try:
-        agent.start()
-        # Keep the main thread running
-        agent.wake_word_thread.join()
-    except KeyboardInterrupt:
-        print("\nStopping Conversation Agent...")
-    finally:
-        agent.stop()
-
-if __name__ == "__main__":
-    main()
